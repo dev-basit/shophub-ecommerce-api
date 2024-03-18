c39ca6135333aeb4af69f87184cd26bee821c971
Abdul Basit <dev.basit2020@gmail.com>
2024-03-18 14:04:01 +0500

    bugs fixed

--- server.js
+++ server.js
@@ -17,11 +17,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Server is ready."));
 
--- utils/token.js
+++ utils/token.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 export const generateToken = (user) => {
   return jwt.sign(
     { _id: user._id, name: user.name, email: user.email, isAdmin: user.isAdmin, isSeller: user.isSeller },
-    process.env.JWT_SECRET || "somethingsecret",
+    process.env.JWT_SECRET,
     { expiresIn: "1d" }
   );
 };
